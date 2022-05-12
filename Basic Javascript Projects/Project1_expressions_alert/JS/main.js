2a90c236db98ad1b94237fede7e7c837b90544ff
WMorf <wesmorford@outlook.com>
2022-05-12 13:48:40 -0700

    Added to main.js

--- Basic Javascript Projects/Project1_expressions_alert/JS/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,27 @@
-alert("Hello World");
-window.alert("Hello World!");
-document.write("Hello World!");
+
+
+var X = "Hello World.";
+var Y = "I made a lot of Dwarf Fortress related HTML pages last course...";
+var z = " Grog: \"I would like to Rage!\"";
+var Z = " Keyleth: " + " \"C\'mon you guys!\"";
+var Group = "Vox Machina", Member1 = "Percy", Member2 = "Vex", Member3 = "Vax";
+
+// Previous 
+//alert("Hello World");
+//window.alert("Hello World!");
+//document.write(X);
+//document.write(z);
+//document.write(Member2);
+
+window.alert(Y);//Makes popup with printed string
+
+document.write("Scanlan: \"BIGBY's HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAND\""
++ "Scanlan cried out as an ethereal purple hand rose to attack!"); //combines 2 strings
+
+document.write(z);
+document.write(3+3); //Expression
+
+function firstFunction() { //Makes a button that changes text
+    var str = 'Some Text!';
+    document.getElementById("Button_Text").innerHTML = str
+}
