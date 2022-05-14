8a7e3d58a56378d676ef8c60cc2743313c09f475
WMorf <wesmorford@outlook.com>
2022-05-14 00:04:51 -0700

    Finished project 5

--- Basic Javascript Projects/Project4_dictionaries/JS/main.js
+++ Basic Javascript Projects/Project4_dictionaries/JS/main.js
@@ -20,3 +20,15 @@ function getDictionary2() {
     delete Monster.Age; //Deletes value
     document.getElementById("Dictionary2").innerHTML = Monster.Age; //returns "undefined"
 }
+
+function SampleOne() {
+    document.write(typeof "Letters"); //returns string
+}
+
+function SampleTwo() {
+    document.write(typeof 5); //returns number
+}
+
+function SampleThree() {
+    document.write("100" + 10); //returns number 10010, combining both without adding the
+}
