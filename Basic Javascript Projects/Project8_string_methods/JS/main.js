db3853e1a0db543d6ba82d16555a38f8731a6db9
WMorf <wesmorford@outlook.com>
2022-05-15 14:26:27 -0700

    ready to submit

--- Basic Javascript Projects/Project8_string_methods/JS/main.js
+++ Basic Javascript Projects/Project8_string_methods/JS/main.js
@@ -11,7 +11,7 @@ function FullSentence() {
 
 //uses slice method to pull specific parts of the string
 function SliceMethod() {
-    var sentence = "C'mon you apes! You wanna live forever?"
+    var sentence = "C'mon you apes! You wanna live forever?";
     var section = sentence.slice(16,30); //slices sentence from character position 16 - 30
     document.getElementById("slice").innerHTML = section; //prints "You wanna live"
 }
@@ -24,8 +24,36 @@ function UpperCase() {
     document.getElementById("uppercase").innerHTML = upperText; //Displays modified Text
 }
 
+//uses Search method
 function CowSearch() {
     var text = document.getElementById("tosearch").innerHTML; //gets text from element
     var searchResults = text.search("cow");
     document.getElementById("searchresult").innerHTML = searchResults; //ZDiisplays position of first occurrence
 }
+
+//uses toString method
+function StringMethod() {
+    var x = 999;
+    document.getElementById("numbers_to_string").innerHTML = x.toString(); //converts 999 to string
+}
+
+//uses toPrecision method
+function PrecisionMethod() {
+    var X = 3.14159265358979323846;
+    document.getElementById("precise").innerHTML = X.toPrecision(5); //formats number to specified length
+}
+
+//uses toFixed method
+function FixNum() {
+    var num = 8.675309;
+    var fixedNum = num.toFixed(2); //converts a number to a string, rounded to a specified number of decimals
+    document.getElementById("tobefixed").innerHTML = fixedNum;
+}
+
+//uses valueOf method
+function GetValue() {
+    var text = "This is a string";
+    var textValue = text.valueOf(); //same as textValue = text
+    document.getElementById("valueof").innerHTML = textValue; //prints value of "text" variable
+}
+
