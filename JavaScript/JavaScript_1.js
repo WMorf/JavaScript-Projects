2f9e4d560a2db1e2731ea13aca450c91ee306b2b
WMorf <wesmorford@outlook.com>
2022-05-18 22:31:45 -0700

    ready to submit

--- JavaScript/JavaScript_1.js
+++ JavaScript/JavaScript_1.js
@@ -47,3 +47,26 @@ function ClassMethod() {
     var X = document.getElementsByClassName("test");
     X[0].innerHTML = "See, it worked!";
 }
+
+// uses canvas to draw a line
+
+function DrawLine() {
+    var c = document.getElementById("canvas");
+    var context = c.getContext("2d");
+    context.moveTo(0,0); //Moves the path to the specified point in the canvas, without creating a line
+    context.lineTo(300, 150); //Adds a new point and creates a line to that point
+    context.stroke(); //draws the path you have defined
+}
+
+// Create Gradient Background
+function GradientBackground() {
+    var c = document.getElementById("canvas2");
+    var context = c.getContext('2d');
+
+    var grid = context.createLinearGradient(0,0, 260, 0); //blend proportions
+    grid.addColorStop(0, "blue"); //Colors to blend
+    grid.addColorStop(1, "yellow");
+
+    context.fillStyle = grid;
+    context.fillRect(1, 1, 297, 147); // fill dimensions
+}
