39ee3aa4c7a257853eb753d66354dd47f3e76b9a
WMorf <wesmorford@outlook.com>
2022-05-20 11:19:32 -0700

    working

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -5,7 +5,8 @@ let activePlayer = 'X';
 let selectedSquares = [];
 
 //displays activePlayer
-document.getElementById('activePlayer').innerHTML = activePlayer + "'s Turn";
+//document.getElementById('activePlayer').innerHTML = activePlayer + "'s Turn";
+
 
 //place x or o on square
 function placeXOrO(squareNumber) {
@@ -29,7 +30,7 @@ function placeXOrO(squareNumber) {
         //concatenated together and added to array
         selectedSquares.push(squareNumber + activePlayer);
 
-        //checkWinConditions();
+        checkWinConditions(); //see if anyone has won
 
         //changes activePlayer
         if (activePlayer === 'X') {
@@ -49,8 +50,86 @@ function placeXOrO(squareNumber) {
 
         if(activePlayer === 'O') {
 
-            disableClick()
+            disableClick() //pauses clicking while it is computer's turn
+
+            setTimeout(function () { computersTurn(); }, 1000) //pauses 1 second before computer goes
 
         }
+
+        return true; //needed for computersTurn
+    }
+
+    //Computer picks a random square
+    function computersTurn() {
+        let success = false //needed for while loop
+
+        let pickASquare; //stores random number between 0 and 8
+
+        while(!success) {
+            pickASquare = String(Math.floor(Math.random() * 9)) //gets random number between 0-8
+
+            if (placeXOrO(pickASquare)) { //checks random number, if true the square hasn't been selected yet
+                placeXOrO(pickASquare); //calls function
+                success = true; //ends loop
+            }
+        }
+    }
+}
+
+
+//Function parses selectedSquares array to search for win conditions.
+//drawWinLine function called if condition is met
+function checkWinConditions() {
+    //X Win
+    // X 0, 1, 2 condition
+    if      (arrayIncludes('0X', '1X', '2X')) { drawEinLine(50, 100, 558, 100) }
+    //  X 3,4,5 condition
+    else if (arrayIncludes('3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304) }
+    //  X 6,7,8 condition
+    else if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508) }
+    // X 0,3,6 condition
+    else if (arrayIncludes('0X', '3X', '6X')) {drawWinLine(100, 50, 100, 558) }
+    // X 1,4,7 condition
+    else if (arrayIncludes('1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558) }
+    // X 2,5,8 condition
+    else if (arrayIncludes('2X', '5X', '8X')) {drawWinLine(508, 50, 508, 558) }
+    // X 6,4,2 condition
+    else if (arrayIncludes('6X', '4X', '2X')) {drawWinLine(100, 508, 510, 90) }
+    // X 0,4,8 condition
+    else if (arrayIncludes('0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520) }
+
+    //O Win
+    //  condition
+    else if (arrayIncludes('0O', '1O', '2O')) {drawWinLine(50, 100, 558, 100) }
+    //  condition
+    else if (arrayIncludes('3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304) }
+    //  condition
+    else if (arrayIncludes('6O', '7O', '8O')) {drawWinLine(50, 508, 558, 508) }
+    //  condition
+    else if (arrayIncludes('0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558) }
+    //  condition
+    else if (arrayIncludes('1O', '4O', '7O')) {drawWinLine(304, 50, 304, 558) }
+    //  condition
+    else if (arrayIncludes('2O', '5O', '8O')) {drawWinLine(508, 50, 508, 558) }
+    //  condition
+    else if (arrayIncludes('6O', '4O', '2O')) {drawWinLine(100, 508, 510, 90) }
+    //  condition
+    else if (arrayIncludes('0O', '4O', '8O')) {drawWinLine(100, 100, 520, 520) }
+
+    else if (selectedSquares.length >= 9) {
+        audio('./media/tie.mp3'); //plays tie sound
+        setTimeout(function () { resetGame(); }, 1000) ; //sets .3 seconds timer before game is reset
+    }
+
+    //function checks if array includes 3 strings. Checks for win condition
+    function arrayIncludes(squareA, squareB, squareC) {
+        //uses variables to check for 3 in a row
+        const a = selectedSquares.includes(squareA)
+        const b = selectedSquares.includes(squareB)
+        const c = selectedSquares.includes(squareC)
+
+
+        //return true if a;; 3 variables are present
+        if (a === true && b === true && c === true) { return true }
     }
 }
